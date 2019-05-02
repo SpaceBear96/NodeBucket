c0c6b6d92ff2339ea04dbfee3184d3765144406d
SpaceBear96 <cafu.aguilar@tecsup.edu.pe>
2019-05-01 23:06:21 -0500

    Commit

---
+++ app/config/s3.env.js
@@ -0,0 +1,8 @@
+const env = {
+	AWS_ACCESS_KEY: 'AKIASWIVSPOOAUMEJ42F',
+	AWS_SECRET_ACCESS_KEY: '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',
+	REGION : 'us-east-2',
+	Bucket: 'jabappdemo'
+};
+
+module.exports = env;
